631fb71bba0d8153dce4cabaf9012854812f4c00
Gianluca <gianluca1020@yahoo.it>
2025-09-19 10:57:25 +0200

    aggiustato il flip card

--- src/app/board/board.ts
+++ src/app/board/board.ts
@@ -1,9 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { Card } from '../interfacce/card';
-import { timer } from 'rxjs';
+import { CommonModule } from '@angular/common';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faUser } from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'app-board',
-  imports: [],
+  imports: [CommonModule,FontAwesomeModule],
   templateUrl: './board.html',
   styleUrls: ['./board.css']
 })
@@ -14,8 +16,11 @@ export class Board implements OnInit {
   primaCardClick!: Card;
   punteggioGiocatore1: number = 0;
   punteggioGiocatore2: number = 0;
-  isFlipped = false;
-  lock = false;
+  isFlipped: boolean = false;
+  lock: boolean = false;
+  faUser= faUser;
+
+  constructor(private cdRef: ChangeDetectorRef) {}
 
   ngOnInit() {
     let seeds: number[] = [];
@@ -29,10 +34,10 @@ export class Board implements OnInit {
     }
     //mischio l ordine dell'array
     for (let seed in seeds) {
-      let indexCasuale = Math.floor(Math.random() * 8);
+      let indexCasuale = Math.floor(Math.random() * seeds.length);
       let varAppoggio = seeds[seed];
       seeds[seed] = seeds[indexCasuale];
-      seeds[indexCasuale] = varAppoggio
+      seeds[indexCasuale] = varAppoggio;
     }
 
     // creo le carte con id unico
@@ -45,71 +50,77 @@ export class Board implements OnInit {
 
 
 
-//   flipCard(id: number) {
-//     const card = this.cards.find(c => c.id === id);
-//      if (!card || card.flipped || this.contatoreClick >= 2) return;
-//       if (card && !card.flipped) {
-//         card.flipped = true;
-//         this.contatoreClick++;
-//         if (this.contatoreClick == 1) {
-//           this.primaCardClick = card;
-//         }
-//         else {
-//           if (card.seed != this.primaCardClick.seed) {
-//             this.contatoreClick++;
-//             card.flipped = false;
-//             this.primaCardClick.flipped = false;
-//             this.contatoreClick = 0;
-//           }
+  //   flipCard(id: number) {
+  //     const card = this.cards.find(c => c.id === id);
+  //      if (!card || card.flipped || this.contatoreClick >= 2) return;
+  //       if (card && !card.flipped) {
+  //         card.flipped = true;
+  //         this.contatoreClick++;
+  //         if (this.contatoreClick == 1) {
+  //           this.primaCardClick = card;
+  //         }
+  //         else {
+  //           if (card.seed != this.primaCardClick.seed) {
+  //             this.contatoreClick++;
+  //             card.flipped = false;
+  //             this.primaCardClick.flipped = false;
+  //             this.contatoreClick = 0;
+  //           }
 
-//           else {
-//             if(this.turnoGiocatore1){
-//               this.punteggioGiocatore1++;
-//             }
-//             else{
-//               this.punteggioGiocatore2++;
-//             }
-//             this.contatoreClick=0;
-//           }
-//       }
+  //           else {
+  //             if(this.turnoGiocatore1){
+  //               this.punteggioGiocatore1++;
+  //             }
+  //             else{
+  //               this.punteggioGiocatore2++;
+  //             }
+  //             this.contatoreClick=0;
+  //           }
+  //       }
+
+  //   }
+
+  // }
+
+  flipCard(id: number) {
+    if (this.lock) return;
+
+    const card = this.cards.find(c => c.id === id);
+
+    if (!card || card.flipped) return;
+
+    card.flipped = true;
+    this.contatoreClick++;
+
+    if (this.contatoreClick === 1) {
+      //mi salvo il seed della prima carta scelta
+      this.primaCardClick = card;
+    } else if (this.contatoreClick === 2) {
+      //controllo dopo aver controllato i due seed
+      this.controllaCoppia(card);
+    }
+  }
+
+  controllaCoppia(secondaCard: Card) {
     
-//   }
-
-// }
-
-flipCard(id: number) {
-  if (this.lock) return;
-
-  const card = this.cards.find(c => c.id === id);
-  if (!card || card.flipped) return;
-
-  card.flipped = true;
-  this.contatoreClick++;
-
-  if (this.contatoreClick === 1) {
-    this.primaCardClick = card;
-  } else if (this.contatoreClick === 2) {
-    this.controllaCoppia(card);
-  }
-}
-
-controllaCoppia(secondaCard: any) {
-  if (secondaCard.seed !== this.primaCardClick.seed) {
-    this.lock = true;
-    // ribalta le carte dopo 2 secondi
-    setTimeout(() => {
-      secondaCard.flipped = false;
-      this.primaCardClick.flipped = false;
+    if (secondaCard.seed !== this.primaCardClick.seed) {
+      this.lock = true;
+      setTimeout(() => {
+        secondaCard.flipped = false;
+        this.primaCardClick.flipped = false;
+        this.contatoreClick = 0;
+        this.lock = false;
+        this.turnoGiocatore1 = !this.turnoGiocatore1;
+        this.cdRef.detectChanges();
+      }, 1000);
+    } else {
+      // carte uguali assegna punteggio
+      if (this.turnoGiocatore1) this.punteggioGiocatore1++;
+      else this.punteggioGiocatore2++;
       this.contatoreClick = 0;
-      this.lock = false;
-    }, 2000);
-  } else {
-    // carte uguali assegna punteggio
-    if (this.turnoGiocatore1) this.punteggioGiocatore1++;
-    else this.punteggioGiocatore2++;
-    this.contatoreClick = 0;
+    }
+
   }
-}
 
 }
 
